b32d2b1c903941f2b6d64dd1fa891a9ef8866b7b
Joffrey Mann <briannaprzybyszbusiness@gmail.com>
2014-09-02 16:41:32 -0400

    Initial Commit

---
+++ GaTechScouting/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GaTechScouting
+//
+//  Created by Joffrey Mann on 9/2/14.
+//  Copyright (c) 2014 JoffreyMann. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
